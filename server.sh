e75034fed9b07fb7bf6d93fcb1646a9ccef9aa20
Jean-Philippe Dutreve <42549240+jdutreve@users.noreply.github.com>
2020-11-15 11:12:02 +0100

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,2 @@
+java -classpath target/classes:/home/jdu/.m2/repository/org/zeromq/jzmq/3.1.0/jzmq-3.1.0.jar:/home/jdu/.m2/repository/org/zeromq/jzmq-jni/3.1.1-SNAPSHOT/jzmq-jni-3.1.1-SNAPSHOT-native-amd64-Linux.jar  Server
+
